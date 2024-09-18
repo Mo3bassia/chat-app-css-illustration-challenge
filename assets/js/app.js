13edd86f5a5626279ac0d1c3c3134d7788fbb773
Mohamed Abassia <mo3bassia@gmail.com>
2024-09-18 21:15:29 +0300

    Ending mobile container and animation by ScrollReveal

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+ScrollReveal().reveal(".mobile-container .left", {
+  origin: "left",
+  distance: "50px",
+  duration: 500,
+  delay: 500,
+  easing: "ease-in-out",
+});
+ScrollReveal().reveal(".mobile-container .right", {
+  origin: "left",
+  distance: "50px",
+  duration: 500,
+  delay: 500,
+  easing: "ease-in-out",
+});
