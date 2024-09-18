4ef4d70a657c985abb6b2d4046f11432897bf9e3
Mohamed Abassia <mo3bassia@gmail.com>
2024-09-18 21:42:27 +0300

    Finish

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,13 @@
 ScrollReveal().reveal(".mobile-container .left", {
   origin: "left",
-  distance: "50px",
+  distance: "20px",
   duration: 500,
   delay: 500,
   easing: "ease-in-out",
 });
 ScrollReveal().reveal(".mobile-container .right", {
-  origin: "left",
-  distance: "50px",
+  origin: "right",
+  distance: "20px",
   duration: 500,
   delay: 500,
   easing: "ease-in-out",
